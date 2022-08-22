41c61d5813f917236698c0ea21282f710953f670
Nikhil Prasad <nikhilprasad1999@gmail.com>
2022-08-22 16:12:07 +0530

    Modified FlightServiceImpl to add deletion features

--- src/main/java/com/capg/service/FlightServiceImpl.java
+++ src/main/java/com/capg/service/FlightServiceImpl.java
@@ -13,14 +13,12 @@ public class FlightServiceImpl implements FlightService{
 
     @Autowired
     private FlightRepository flightRepository;
-
     //Get all flight details
     @Override
     public List<FlightsDTO> getFlights() {
         List<Flights> flights = flightRepository.findAll();
         return flights.stream().map(FlightsDTO::new).collect(Collectors.toList());
     }
-
     //Find flight by ID
     @Override
     public FlightsDTO getFlight(Integer id) {
@@ -28,14 +26,12 @@ public class FlightServiceImpl implements FlightService{
                 .orElseThrow(() -> new FlightNotFoundException("Flight does not exist with id: " + id));
         return new FlightsDTO(flights);
     }
-
     //Create new flight
     @Override
     public FlightsDTO newFlight(FlightsDTO flightsDTO) {
         Flights flights = new Flights(flightsDTO);
         return new FlightsDTO(flightRepository.save(flights));
     }
-
     //Update flight
     @Override
     public FlightsDTO updateFlight(Integer id, FlightsDTO flightsDTO) {
@@ -52,14 +48,16 @@ public class FlightServiceImpl implements FlightService{
 
         return new FlightsDTO(flights);
     }
-
+    //Delete flight with given ID
     @Override
     public void deleteFlight(Integer id) {
-
+        Flights flights = flightRepository.findById(id)
+                .orElseThrow(() -> new FlightNotFoundException("Flight does not exist with id: " + id));
+        flightRepository.delete(flights);
     }
-
+    //Delete all flights
     @Override
     public void deleteAll() {
-
+        flightRepository.deleteAll();
     }
 }
